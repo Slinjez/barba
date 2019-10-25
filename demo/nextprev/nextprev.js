d19226ef3ae76c4dd343d90247d79dc41ed35d70
Thierry Michel <thmichel@gmail.com>
2019-10-25 09:58:16 +0200

    :sparkles: add nextprev demo

--- demo/nextprev/nextprev.js
+++ demo/nextprev/nextprev.js
@@ -1,81 +1,56 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var lastElementClicked;
-  var PrevLink = document.querySelector('a.prev');
-  var NextLink = document.querySelector('a.next');
+/* global barba, TweenLite */
+document.addEventListener('DOMContentLoaded', () => {
+  const prevLink = document.querySelector('a.prev');
+  const nextLink = document.querySelector('a.next');
 
-  Barba.Pjax.init();
-  Barba.Prefetch.init();
-
-  Barba.Dispatcher.on('linkClicked', function(el) {
-    lastElementClicked = el;
-  });
-
-  var MovePage = Barba.BaseTransition.extend({
-    start: function() {
-      this.originalThumb = lastElementClicked;
-
-      Promise
-        .all([this.newContainerLoading, this.scrollTop()])
-        .then(this.movePages.bind(this));
-    },
-
-    scrollTop: function() {
-      var deferred = Barba.Utils.deferred();
-      var obj = { y: window.pageYOffset };
-
-      TweenLite.to(obj, 0.4, {
-        y: 0,
-        onUpdate: function() {
-          if (obj.y === 0) {
-            deferred.resolve();
-          }
-
-          window.scroll(0, obj.y);
-        },
-        onComplete: function() {
-          deferred.resolve();
-        }
+  const move = (container, direction, xPercent) =>
+    new Promise(resolve => {
+      TweenLite[direction](container, 0.6, {
+        xPercent,
+        clearProps: 'all',
+        onComplete: resolve,
       });
+    });
 
-      return deferred.promise;
-    },
-
-    movePages: function() {
-      var _this = this;
-      var goingForward = true;
-      this.updateLinks();
-
-      if (this.getNewPageFile() === this.oldContainer.dataset.prev) {
-        goingForward = false;
-      }
-
-      TweenLite.set(this.newContainer, {
-        visibility: 'visible',
-        xPercent: goingForward ? 100 : -100,
+  const movePageTransition = {
+    sync: true,
+    before: () =>
+      new Promise(resolve => {
+        TweenLite.to(window, 0.4, {
+          scrollTo: 0,
+          onUpdate() {
+            window.pageYOffset === 0 && resolve();
+          },
+          onComplete: resolve,
+        });
+      }),
+    leave: ({ current, trigger }) =>
+      move(
+        current.container,
+        'to',
+        trigger.classList.contains('next') ? -100 : 100
+      ),
+    enter({ next, trigger }) {
+      TweenLite.set(next.container, {
         position: 'fixed',
         left: 0,
         top: 0,
-        right: 0
+        right: 0,
       });
 
-      TweenLite.to(this.oldContainer, 0.6, { xPercent: goingForward ? -100 : 100 });
-      TweenLite.to(this.newContainer, 0.6, { xPercent: 0, onComplete: function() {
-        TweenLite.set(_this.newContainer, { clearProps: 'all' });
-        _this.done();
-      }});
+      return move(
+        next.container,
+        'from',
+        trigger.classList.contains('next') ? 100 : -100
+      );
     },
-
-    updateLinks: function() {
-      PrevLink.href = this.newContainer.dataset.prev;
-      NextLink.href = this.newContainer.dataset.next;
+    after({ next }) {
+      prevLink.href = next.container.dataset.prev;
+      nextLink.href = next.container.dataset.next;
     },
-
-    getNewPageFile: function() {
-      return Barba.HistoryManager.currentStatus().url.split('/').pop();
-    }
-  });
-
-  Barba.Pjax.getTransition = function() {
-    return MovePage;
   };
+
+  barba.init({
+    transitions: [movePageTransition],
+  });
 });
