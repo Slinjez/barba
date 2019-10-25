de8d7374b994abba59db8d3b1eae278b84a5534b
Thierry Michel <thmichel@gmail.com>
2019-10-25 11:07:51 +0200

    :sparkles: add nextprev-circle demo

--- demo/nextprev-circle/nextprev.js
+++ demo/nextprev-circle/nextprev.js
@@ -1,116 +1,96 @@
-document.addEventListener("DOMContentLoaded", function() {
-  if (!('webkitClipPath' in document.body.style)) {
-    alert('Sorry, this demo is available just with webkitClipPath. Try with Chrome/Safari.');
+/* global barba, TweenLite, TimelineMax */
+document.addEventListener('DOMContentLoaded', () => {
+  if (!('clipPath' in document.body.style)) {
+    // eslint-disable-next-line no-alert
+    alert(
+      'Sorry, this demo is available just with "clip-path" property. Try another browser…'
+    );
   }
 
-  Barba.Pjax.init();
-  Barba.Prefetch.init();
+  let isAnimating = false;
+  // eslint-disable-next-line no-extra-parens
+  const getXCoord = el => (el.classList.contains('next') ? '97' : '3');
+  const tweenMaskTo = (el, radius) => {
+    if (isAnimating || !el.classList.contains('nav')) {
+      return;
+    }
 
-  var isAnimating = false;
+    TweenLite.to(el, 0.3, {
+      webkitClipPath: `circle(${radius}px at ${getXCoord(el)}% 50%)`,
+    });
+  };
 
-  document.addEventListener('mouseover', function(e) {
+  document.addEventListener('mouseover', e => {
     tweenMaskTo(e.target, 100);
   });
 
-  document.addEventListener('mouseout', function(e) {
+  document.addEventListener('mouseout', e => {
     tweenMaskTo(e.target, 40);
   });
 
-  var tweenMaskTo = function(el, radius) {
-    if (isAnimating || !el.classList.contains('nav'))
-      return;
-
-    var xCoord = el.classList.contains('next') ? '97' : '3';
-    TweenLite.to(el, 0.3, { webkitClipPath: 'circle(' + radius + 'px at ' + xCoord + '% 50%)' });
-  };
-
-  var CoverMaskTransition = Barba.BaseTransition.extend({
-    start: function() {
+  const coverMaskTransition = {
+    leave({ current, trigger }) {
       isAnimating = true;
 
-      Promise
-        .all([this.newContainerLoading, this.scrollTop()])
-        .then(this.display.bind(this));
-    },
+      const done = this.async();
+      const { container } = current;
+      const { innerWidth } = window;
+      const $text = container.querySelector('.text');
+      const tl = new TimelineMax({
+        onComplete: done,
+      });
 
-    scrollTop: function() {
-      var deferred = Barba.Utils.deferred();
-      var obj = { y: window.pageYOffset };
-
-      TweenLite.to(obj, 0.4, {
-        y: 0,
-        onUpdate: function() {
-          if (obj.y === 0) {
-            deferred.resolve();
-          }
-
-          window.scroll(0, obj.y);
+      TweenLite.set(trigger, { zIndex: 100 });
+      tl.add('start').to($text, 0.3, { opacity: 0 }, 'start');
+      tl.to(
+        trigger,
+        1,
+        {
+          webkitClipPath: `circle(${innerWidth}px at ${getXCoord(
+            trigger
+          )}% 50%)`,
         },
-        onComplete: function() {
-          deferred.resolve();
-        }
-      });
-
-      return deferred.promise;
+        'start'
+      );
     },
-
-    getNewPageFile: function() {
-      return Barba.HistoryManager.currentStatus().url.split('/').pop();
-    },
-
-    getLinkByHref: function(href) {
-      return document.querySelector('a[href="' + href + '"]');
-    },
-
-    display: function() {
-      var _this = this;
-      var tl = new TimelineMax({
-        onComplete: function() {
-          _this.newContainer.style.position = 'static';
-          _this.done();
+    enter({ next }) {
+      const done = this.async();
+      const { container } = next;
+      const $text = container.querySelector('.text');
+      const $newLinkPrev = container.querySelector('.nav.prev');
+      const $newLinkNext = container.querySelector('.nav.next');
+      const tl = new TimelineMax({
+        onComplete() {
           isAnimating = false;
-        }
+          done();
+        },
       });
 
-      var wWidth = window.innerWidth;
-      var oldText = this.oldContainer.querySelector('.text');
-      var newText = this.newContainer.querySelector('.text');
-      var newLinkPrev = this.newContainer.querySelector('.nav.prev');
-      var newLinkNext = this.newContainer.querySelector('.nav.next');
-
-      var linkElement = this.getLinkByHref(this.getNewPageFile());
-      var xCoord = linkElement.classList.contains('next') ? '97' : '3';
-
-      TweenLite.set(linkElement, { zIndex: 100 });
-
-      TweenLite.set(this.newContainer, {
+      TweenLite.set(container, {
         position: 'fixed',
-        visibility: 'visible',
+        left: 0,
         top: 0,
         right: 0,
-        bottom: 0,
-        left: 0,
-        opacity: 0,
-        zIndex: 200
       });
 
-      TweenLite.set(newText, { opacity: 0 });
-      TweenLite.set(newLinkPrev, { webkitClipPath: 'circle(0px at 3% 50%)' });
-      TweenLite.set(newLinkNext, { webkitClipPath: 'circle(0px at 97% 50%)' });
-
-      tl.add('start');
-      tl.to(oldText, 0.3, { opacity: 0 }, 'start');
-      tl.to(linkElement, 1, { webkitClipPath: 'circle(' + wWidth + 'px at ' + xCoord + '% 50%)' }, 'start');
-      tl.to(this.newContainer, 0.3, { opacity: 1 });
-      tl.to(newText, 0.3, { opacity: 1 });
-
-      tl.add('nextLinks');
-      tl.to(newLinkPrev, 0.2, { webkitClipPath: 'circle(40px at 3% 50%)' }, 'nextLinks');
-      tl.to(newLinkNext, 0.2, { webkitClipPath: 'circle(40px at 97% 50%)' }, 'nextLinks');
-    }
-  });
-
-  Barba.Pjax.getTransition = function() {
-    return CoverMaskTransition;
+      tl.from($text, 0.3, { opacity: 0 })
+        .add('nextLinks')
+        .to(
+          $newLinkPrev,
+          0.2,
+          { webkitClipPath: 'circle(40px at 3% 50%)' },
+          'nextLinks'
+        )
+        .to(
+          $newLinkNext,
+          0.2,
+          { webkitClipPath: 'circle(40px at 97% 50%)' },
+          'nextLinks'
+        );
+    },
   };
+
+  barba.init({
+    transitions: [coverMaskTransition],
+  });
 });
